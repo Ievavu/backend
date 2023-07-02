a69f7881cffcf4990659f83b7bdf376fabf17196
Ieva Vidunaite-Urbaniene <i.vidunaite@gmail.com>
2023-07-02 08:58:36 +0300

    initial setup

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+var cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose
+  .connect(
+    process.env.MONGO_CONNECT
+  )
+  .then(() => {
+    console.log("CONNECTED");
+  })
+  .catch((err) => {
+    console.log("err", err);
+  });
+
+app.listen(process.env.PORT, () => {
+  console.log("Your app is alive!!!!!");
+});
